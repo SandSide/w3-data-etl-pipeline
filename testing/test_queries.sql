6d544248e1bd0068e5842ab19b4e3a518b3ecf02
SandSide <37347875+SandSide@users.noreply.github.com>
2024-04-05 03:51:44 +0100

    test: Write sql queries to test fact table

--- testing/test_queries.sql
+++ testing/test_queries.sql
@@ -2,3 +2,26 @@ SELECT *
 FROM log_fact_table as f
 INNER JOIN dim_ip as d ON f.ip_id = d.ip_id
 WHERE d.country_code = 'GB';
+
+-- 
+SELECT d.year, d.quarter, COUNT(*) 
+FROM log_fact_table as f
+INNER JOIN dim_date as d ON f.date_id = d.date_id
+WHERE is_bot = 'f'
+GROUP BY d.year, d.quarter
+ORDER BY d.year ASC, d.quarter ASC;
+
+
+-- 
+SELECT d.hour, COUNT(*) 
+FROM log_fact_table as f
+INNER JOIN dim_time as d ON f.time_id = d.time_id
+WHERE is_bot = 'f'
+GROUP BY d.hour
+ORDER BY d.hour ASC;
+
+SELECT d.week_day, COUNT(*) 
+FROM log_fact_table as f
+INNER JOIN dim_time as d ON f.time_id = d.time_id
+WHERE is_bot = 'f'
+GROUP BY d.week_day
