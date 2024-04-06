9532f36a0b2c96131261edba9118c2c2c33e38c0
SandSide <37347875+SandSide@users.noreply.github.com>
2024-04-06 05:16:20 +0100

    refactor: Separate file path tasks

--- src/dags/w3c.py
+++ src/dags/w3c.py
@@ -27,91 +27,8 @@ from ip_tasks import define_ip_tasks
 from date_tasks import define_date_tasks
 from browser_tasks import define_browser_tasks
 from os_tasks import define_os_tasks
+from file_path_tasks import define_file_path_tasks
 
-
-def extract_file_details():
-    try:
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        
-        # Add columns to file table
-        sql_query = '''
-            ALTER TABLE staging_file
-            ADD COLUMN IF NOT EXISTS file_path VARCHAR,
-            ADD COLUMN IF NOT EXISTS file_name VARCHAR,
-            ADD COLUMN IF NOT EXISTS file_extension VARCHAR,
-            ADD COLUMN IF NOT EXISTS file_directory VARCHAR;
-            '''
-        cursor.execute(sql_query)
-        
-        cursor.execute('SELECT file_id, raw_file_path FROM staging_file;')
-        result = cursor.fetchall()
-        
-        for row in result:
-            file_id, file_path = row
-            
-            values = process_file_path(file_path)
-            
-            # Insert file details
-            cursor.execute('''
-                    UPDATE staging_file
-                    SET file_path = %s, file_name = %s, file_extension = %s, file_directory = %s
-                    WHERE  file_id = %s;
-                    ''', (*values, file_id))
-            
-        conn.commit()
-            
-    except Exception as e:
-        conn.rollback()
-        logging.exception(f'Error: {e}')
-        raise
-    
-    finally:
-        cursor.close()
-        conn.close()
-        
-
-def process_file_path(raw_file_path):
-
-    file_directory, file_name = os.path.split(raw_file_path)
-    
-    # Remove anything after +++
-    if '+++' in file_name:
-        i = file_name.find('+++')
-        file_name = file_name[:i]
-       
-    
-    # Remove anything after " 
-    if '"' in file_name:
-        i = file_name.find('"')
-        file_name = file_name[:i]
-    
-    
-    # Remove anything after ?
-    if '?' in file_name:
-        i = file_name.find('?')
-        file_name = file_name[:i]
-    
-    
-    pattern = r'[^a-zA-Z0-9./\-\'+_]'
-    file_name = re.sub(pattern, '', file_name)
-
-    a, file_extension = os.path.splitext(file_name)    
-    
-    if '+' in file_extension:
-        file_extension = ''
-    
-
-    if file_directory.endswith('/'):
-        file_path = f'{file_directory}{file_name}' 
-    else:
-        file_path = f'{file_directory}/{file_name}' 
-          
-    
-    # out = (raw_file_path, file_path, file_name, file_extension, file_directory)   
-        
-    return (file_path, file_name, file_extension, file_directory)  
-        
 
 with DAG(
     dag_id = 'Process_W3_Data',                          
@@ -133,41 +50,9 @@ with DAG(
 
     determine_os_task, extract_unique_os_task, build_dim_os_table_task = define_os_tasks(dag)
  
-    ## FILE TASKS
-    
-    extract_unique_file_path_task = PostgresOperator(
-        task_id = 'extract_unique_file_path',
-        sql = 
-        '''
-            DROP TABLE IF EXISTS staging_file;
-            
-            CREATE TABLE staging_file (
-                file_id SERIAL PRIMARY KEY,
-                raw_file_path VARCHAR
-            );
-            
-            INSERT INTO staging_file (raw_file_path)
-            SELECT DISTINCT raw_file_path from staging_log_data;
-        '''
-    )
-    
-    extract_file_details_task = PythonOperator(
-        task_id = 'extract_file_details',
-        python_callable = extract_file_details,
-    )
-    
-    
-    build_dim_file_table_task = PostgresOperator(
-        task_id = 'build_dim_file_table',
-        sql = 
-        '''
-            DROP TABLE IF EXISTS dim_file;
-            
-            CREATE TABLE dim_file AS
-            SELECT * FROM staging_file;
-        '''
-    )
-    
+    extract_unique_file_path_task, extract_file_details_task, build_dim_file_table_task = define_file_path_tasks(dag)
+
+
     ## TIME TASKS
     extract_unique_time_task = PostgresOperator(
         task_id = 'extract_unique_time',
