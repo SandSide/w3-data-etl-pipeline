cbec3d192dd4a4e5ffab10d9fc2bdb756196d0fd
SandSide <37347875+SandSide@users.noreply.github.com>
2024-04-09 01:35:10 +0100

    refactor: Only determine device type

--- src/common_package/device_tasks.py
+++ src/common_package/device_tasks.py
@@ -7,28 +7,26 @@ from common_package.db_conn import get_db_connection
 from user_agents import parse
 import logging
 
+add_device_column_query = '''
+    ALTER TABLE staging_log_data
+    ADD COLUMN IF NOT EXISTS device_type VARCHAR;
+'''
+
 update_with_device_details_query = '''
     UPDATE staging_log_data
     SET 
-        device_family = %s, 
-        device_model = %s,
         device_type = %s
     WHERE  
         log_id = %s;
 '''
+
 
 def determine_device_details():
     try:
         conn = get_db_connection()
         cursor = conn.cursor()
         
-        sql_query = '''
-            ALTER TABLE staging_log_data
-            ADD COLUMN IF NOT EXISTS device_family VARCHAR,
-            ADD COLUMN IF NOT EXISTS device_model VARCHAR,
-            ADD COLUMN IF NOT EXISTS device_type VARCHAR;
-        '''
-        cursor.execute(sql_query)
+        cursor.execute(add_device_column_query)
         
         sql_query = 'SELECT log_id, browser_string FROM staging_log_data'
         cursor.execute(sql_query)
@@ -40,7 +38,7 @@ def determine_device_details():
             
             values = determine_details(browser_string)
 
-            cursor.execute(update_with_device_details_query, (*values, log_id))              
+            cursor.execute(update_with_device_details_query, (values, log_id))              
     
         conn.commit()
                
@@ -57,11 +55,13 @@ def determine_details(browser_string):
     
     parsed_ua = parse(browser_string)
     
-    device_family = parsed_ua.device.family or 'Unknown'
-    device_model = parsed_ua.device.model or 'Unknown'
+    # device_family = parsed_ua.device.family or 'Unknown'
+    # device_model = parsed_ua.device.model or 'Unknown'
     device_type = determine_device_type(parsed_ua) or 'Unknown'
 
-    return device_family, device_model, device_type
+    # return device_family, device_model, device_type
+    
+    return device_type
 
 
 def determine_device_type(ua):
@@ -72,3 +72,7 @@ def determine_device_type(ua):
         return 'Tablet'
     elif ua.is_pc:
         return 'PC'
+    
+    
+extract_unique_device_query = '''
+'''
